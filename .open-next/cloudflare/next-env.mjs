5ab03ea9f2bcf52ec3c8db17ccec19426da2bdf8
MohamedAmineBayar1 <aminebayar811@gmail.com>
2026-03-26 16:32:19 +0100

    cleanup: remove .open-next from git tracking and update gitignore

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"TMDB_API_KEY":"85e9c69c4ca390d9f14960b8c01ece60"};
-export const development = {"TMDB_API_KEY":"85e9c69c4ca390d9f14960b8c01ece60"};
-export const test = {};
